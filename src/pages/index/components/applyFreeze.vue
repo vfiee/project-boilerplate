46b840a38ec26b863006cbfe7494f134169e01e3
vyron <fiee_vyron@163.com>
2025-04-21 14:59:06 +0800

    feat: 对接申请冻结,战败,保存跟进等接口

--- src/pages/index/components/applyFreeze.vue
+++ src/pages/index/components/applyFreeze.vue
@@ -1,20 +1,46 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { Icon } from '@iconify/vue'
+import { getUrlParams } from '@/utils'
+import { useAxios } from '@/services'
 
 defineOptions({ name: 'ApplyFreeze' })
 
 const visible = defineModel('visible')
 
-const model = ref({})
+const model = ref({
+  ReasonForFreezing__c: true
+})
 
-const handleSubmit = () => {
-  console.log('提交')
+watch(
+  () => model.value.ReasonForFreezing__c,
+  (val) => {
+    if (val) {
+      model.value.remarks = ''
+    }
+  }
+)
+
+const { execute, isLoading } = useAxios(
+  '/rest/data/v2.0/scripts/api/central/createFirstMaintenanceLeadFollowupRecords',
+  {
+    method: 'POST'
+  }
+)
+const handleSubmit = async () => {
+  const { recordId } = getUrlParams()
+  await execute({ data: { dataId: recordId, status: 6, ...model.value } })
   visible.value = false
+  window.$message.success('申请冻结成功')
 }
 </script>
 <template>
-  <a-modal v-model:open="visible" title="申请冻结" @ok="handleSubmit">
+  <a-modal
+    v-model:open="visible"
+    title="申请冻结"
+    @ok="handleSubmit"
+    :confirm-loading="isLoading"
+  >
     <a-form v-model:model="model" labelAlign="left" :labelCol="{ span: 4 }">
       <div class="my-20px bg-pink-100 p-10px text-red flex-y-center rounded">
         <span class="whitespace-nowrap">
@@ -26,20 +52,29 @@ const handleSubmit = () => {
         />
       </div>
       <a-form-item name="other">
-        <a-checkbox v-model:checked="model.other2">客户拒绝打扰</a-checkbox>
-      </a-form-item>
-      <a-row gutter="10">
-        <a-col>
-          <a-form-item name="other">
-            <a-checkbox v-model:checked="model.other">其他</a-checkbox>
-          </a-form-item></a-col
+        <a-radio-group
+          class="w-full"
+          v-model:value="model.ReasonForFreezing__c"
         >
-        <a-col>
-          <a-form-item name="reason">
-            <a-input :disabled="!model.other" placeholder="可备注具体原因" />
-          </a-form-item>
-        </a-col>
-      </a-row>
+          <a-row>
+            <a-col>
+              <a-radio :value="true"> 客户拒绝打扰 </a-radio>
+            </a-col>
+          </a-row>
+          <a-row class="mt-30px">
+            <a-col span="4">
+              <a-radio :value="false"> 其它 </a-radio>
+            </a-col>
+            <a-col span="20">
+              <a-textarea
+                v-model:value="model.remarks"
+                :disabled="model.ReasonForFreezing__c"
+                placeholder="可备注具体原因"
+              />
+            </a-col>
+          </a-row>
+        </a-radio-group>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
--- src/pages/index/components/failReason.vue
+++ src/pages/index/components/failReason.vue
@@ -1,37 +1,57 @@
 <script setup>
+import { useAntdForm, useFormRules } from '@/hooks'
 import { ref } from 'vue'
+import { useAxios } from '@/services'
+import { getUrlParams } from '@/utils'
 
 defineOptions({ name: 'FailReasonModal' })
 
-const visible = defineModel('visible')
-
 const model = ref({})
+const visible = defineModel('visible')
+const { defaultRequiredRule } = useFormRules()
+const { formRef, resetFields, validate } = useAntdForm()
 
-const handleSubmit = () => {
-  console.log('提交')
+const rules = {
+  ReasonForFreezing__c: defaultRequiredRule
+}
+
+const { execute, isLoading } = useAxios(
+  '/rest/data/v2.0/scripts/api/central/createFirstMaintenanceLeadFollowupRecords',
+  {
+    method: 'POST'
+  }
+)
+
+const handleSubmit = async () => {
+  await validate()
+  const { recordId } = getUrlParams()
+  await execute({ data: { dataId: recordId, ...model.value, status: 4 } })
   visible.value = false
+  resetFields()
+  window.$message.success('申请战败成功')
 }
 </script>
 <template>
-  <a-modal v-model:open="visible" title="战败原因" @ok="handleSubmit">
+  <a-modal
+    v-model:open="visible"
+    title="战败原因"
+    @ok="handleSubmit"
+    :confirm-loading="isLoading"
+  >
     <a-form
-      v-model:model="model"
+      ref="formRef"
+      :model="model"
       class="mt-30px"
       labelAlign="left"
+      :rules="rules"
       :labelCol="{ span: 4 }"
     >
-      <a-row gutter="10">
-        <a-col>
-          <a-form-item name="other">
-            <a-checkbox v-model:checked="model.other">其他</a-checkbox>
-          </a-form-item>
-        </a-col>
-        <a-col>
-          <a-form-item name="reason">
-            <a-input placeholder="可备注具体原因" />
-          </a-form-item>
-        </a-col>
-      </a-row>
+      <a-form-item name="reason">
+        <a-textarea
+          v-model:value="model.ReasonForFreezing__c"
+          placeholder="可备注具体原因"
+        />
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
